97fd1c786a456e56c08126aa3e71ccf82c6071f1
MPS Vihanha <>
2024-02-23 22:28:54 +0530

    removing bilderplate code final

--- script.js
+++ script.js
@@ -2,17 +2,22 @@ const model = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
 const btnCloseModel = document.querySelector('.close-modal');
 const btnOpenModel = document.querySelectorAll('.show-modal');
-console.log(btnOpenModel);
 
-for (let i = 0; i < btnOpenModel.length; i++) {
-  btnOpenModel[i].addEventListener('click', function () {
-    console.log('Button clicked !');
-    model.classList.remove('hidden');
-    overlay.classList.remove('hidden');
-  });
-}
-
-btnCloseModel.addEventListener('click', function () {
+const closeModal = function () {
   model.classList.add('hidden');
   overlay.classList.add('hidden');
-});
+};
+
+const openModal = function () {
+  console.log('Button clicked !');
+  model.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
+
+for (let i = 0; i < btnOpenModel.length; i++) {
+  btnOpenModel[i].addEventListener('click', openModal);
+}
+
+btnCloseModel.addEventListener('click', closeModal);
+
+overlay.addEventListener('click', closeModal);
